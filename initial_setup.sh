cffaaf77f9017eef6ed7b4cf4aae351a2e341f0a
Richyread <7470850+Richyread@users.noreply.github.com>
2025-02-13 15:05:53 +0000

    Add files via upload
    Script for installation of Certbot and links

---
+++ initial_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xe
+# -e = exit if any command execution returns nonzero status except in a context that handles such: while until if && || !
+# -x = print commands as they are executed
+
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+sudo snap set certbot trust-plugin-with-root=ok
+sudo snap install certbot-dns-cloudflare
+
+exit 0
